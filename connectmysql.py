142b8369fa4053e397fa65b8ca1fe89fd4c0a7dd
BO <napawan-bo@hotmail.com>
2023-09-20 15:32:19 +0700

    edit connection

--- connectmysql.py
+++ connectmysql.py
@@ -7,18 +7,30 @@ import connectmysql as con
 # สร้างฟังก์ชั่นสำหรับเชื่อมDB mysql
 
 
+# def connectdb():
+#     connectdb = pymysql.connect(
+#         host='localhost',
+#         user='root',
+#         password='',
+#         db='pythondb',
+#         port=3306,
+#         cursorclass=pymysql.cursors.DictCursor,
+#         charset='utf8'
+#     )
+#     return connectdb
+
+
+# call function
+# print(connectdb())
+
 def connectdb():
     connectdb = pymysql.connect(
-        host='localhost',
-        user='root',
-        password='',
-        db='pythondb',
+        host='bhkc05crhs7zigzyxwkt-mysql.services.clever-cloud.com',
+        user='u7pnpbjt231tuuwn',
+        password='wWtprfw7u8RxkekYkM1F',
+        db='bhkc05crhs7zigzyxwkt',
         port=3306,
         cursorclass=pymysql.cursors.DictCursor,
         charset='utf8'
     )
     return connectdb
-
-
-# call function
-# print(connectdb())
